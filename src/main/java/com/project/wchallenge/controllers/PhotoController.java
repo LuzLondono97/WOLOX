21ac483492cc5bbb1c594df46203719da9499323
LuzLondono97 <luza0748@gmail.com>
2021-04-26 00:03:19 -0500

    Creation for get the Photos Rest Template

--- src/main/java/com/project/wchallenge/controllers/PhotoController.java
+++ src/main/java/com/project/wchallenge/controllers/PhotoController.java
@@ -5,6 +5,7 @@ import com.project.wchallenge.services.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,19 @@ public class PhotoController {
     @Autowired
     private PhotoService photoService;
 
-    @GetMapping("/getPhotosTemplate")
+    @GetMapping("/getAllPhotosTemplate")
     private ResponseEntity<List<Photo>> getPhotosTemplate() {
-        return ResponseEntity.ok(photoService.getPhotosTemplate());
+        return ResponseEntity.ok(photoService.getAllPhotosTemplate());
+    }
+
+    @GetMapping("getPhotoByIdTemplate/{id}")
+    public ResponseEntity<Photo> getPhotoByIdTemplate(@PathVariable(value = "id") Long photoId) {
+        return ResponseEntity.ok(photoService.getPhotoByIdTemplate(photoId));
+    }
+
+    @GetMapping("getPhotosByUserIdTemplate/{id}")
+    public ResponseEntity<List<Photo>> getPhotosByUserIdTemplate(@PathVariable(value = "id") Long photoAlbumId) {
+        return ResponseEntity.ok(photoService.getPhotosByUserIdTemplate(photoAlbumId));
     }
 
 }
--- src/main/java/com/project/wchallenge/services/PhotoService.java
+++ src/main/java/com/project/wchallenge/services/PhotoService.java
@@ -1,26 +1,64 @@
 package com.project.wchallenge.services;
 
 import com.project.wchallenge.models.Photo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class PhotoService {
 
     private static final RestTemplate restTemplate = new RestTemplate();
+    private final AlbumService albumService;
+    String urlPhoto = "https://jsonplaceholder.typicode.com/photos";
+    String urlPhotoByAlbumId = urlPhoto + "?albumId=";
 
-    public List<Photo> getPhotosTemplate() {
+    @Autowired
+    public PhotoService(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
+    public List<Photo> getAllPhotosTemplate() {
         ResponseEntity<Photo[]> response =
                 restTemplate.getForEntity(
-                        "https://jsonplaceholder.typicode.com/photos/",
+                        urlPhoto,
                         Photo[].class);
         Photo[] photos = response.getBody();
         List<Photo> photosList = Arrays.asList(photos);
         return photosList;
+    }
+
+    public Photo getPhotoByIdTemplate(Long photoId) {
+        ResponseEntity<Photo> response = restTemplate.exchange(
+                urlPhoto + "/" + photoId.toString(),
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<Photo>() {
+                }
+        );
+        return response.getBody();
+    }
+
+    public List<Photo> getPhotosByUserIdTemplate(Long photoAlbumId) {
+        List<Photo> photos = new ArrayList<>();
+        albumService.getAlbumsByUserIdTemplate(photoAlbumId).forEach(albumId -> {
+            photos.addAll(
+                    Objects.requireNonNull(restTemplate.exchange(
+                            (urlPhotoByAlbumId + albumId.getId()),
+                            HttpMethod.GET, null,
+                            new ParameterizedTypeReference<List<Photo>>() {
+                            }
+                    ).getBody())
+            );
+        });
+        return photos;
     }
 
 }
