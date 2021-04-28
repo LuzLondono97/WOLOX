1692bb9d7764855bfd6c98c5b0a9d41849b68a6e
LuzLondono97 <luza0748@gmail.com>
2021-04-28 17:44:52 -0500

    Creation of the method for get the users by album and permissions

--- src/main/java/com/project/wchallenge/controllers/AlbumUserController.java
+++ src/main/java/com/project/wchallenge/controllers/AlbumUserController.java
@@ -1,15 +1,18 @@
 package com.project.wchallenge.controllers;
 
 import com.project.wchallenge.models.AlbumUser;
+import com.project.wchallenge.models.User;
 import com.project.wchallenge.services.AlbumUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/albums")
@@ -33,6 +36,15 @@ public class AlbumUserController {
             return ResponseEntity.ok(albumUserService.updateAlbumPermissions(albumId, userId, accessTypeId));
         } catch (Exception e) {
             throw new Exception("No tiene permisos para modificar el Album Usuario");
+        }
+    }
+
+    @GetMapping("/getUsersPermissionsAlbum")
+    public ResponseEntity<List<User>> getUsersPermissionsAlbum(@PathParam("albumId") Long albumId, @PathParam("accessTypeId") Long accessTypeId) throws Exception {
+        try {
+            return ResponseEntity.ok(albumUserService.getUsersPermissionsAlbum(albumId, accessTypeId));
+        } catch (Exception e) {
+            throw new Exception("No tiene permisos para consultar el Usuario");
         }
     }
 
